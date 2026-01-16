c662db1f2d81f35c40eb2c04d1ecc27fcc70b456
Issiwara Kavinda <issiwarakavinda@Issiwaras-MacBook-Air.local>
2026-01-16 19:23:09 +0100

    infra setup

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
+resource "aws_security_group" "allow_8000" {
+  name        = "allow-8000"
+  description = "Allow port 8000"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow app on port 8000"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "qa-allow-8000"
+  }
+}
+
+
+
+
+resource "aws_instance" "ubuntu_ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_8000.id]
+
+  tags = {
+    Name        = "ubuntu-ec2-qa"
+    Environment = "QA"
+  }
+}
